545cc22f3593e2f23bfa1eca01a979d394ce75cb
Jerrycull00 <onaolapojeremiah114@gmail.com>
2023-07-18 16:56:20 -0700

    Practice Space

---
+++ simple_shell_practice/parser.c
@@ -0,0 +1,86 @@
+#include "shell.h"
+
+/**
+ * _is_cmd_ - determines if a file is an executable command
+ * @info: the info struct
+ * @path: path to the file
+ *
+ * Return: 1 if true, 0 otherwise
+ */
+int _is_cmd_(info_t *info, char *path)
+{
+	struct stat st;
+
+	(void)info;
+	if (!path || stat(path, &st))
+		return (0);
+
+	if (st.st_mode & S_IFREG)
+	{
+		return (1);
+	}
+	return (0);
+}
+
+/**
+ * _dup_chars_ - duplicates characters
+ * @pathstr: the PATH string
+ * @start: starting index
+ * @stop: stopping index
+ *
+ * Return: pointer to new buffer
+ */
+char *_dup_chars_(char *pathstr, int start, int stop)
+{
+	static char buf[1024];
+	int i = 0, k = 0;
+
+	for (k = 0, i = start; i < stop; i++)
+		if (pathstr[i] != ':')
+			buf[k++] = pathstr[i];
+	buf[k] = 0;
+	return (buf);
+}
+
+/**
+ * _find_path_ - finds this cmd in the PATH string
+ * @info: the info struct
+ * @pathstr: the PATH string
+ * @cmd: the cmd to find
+ *
+ * Return: full path of cmd if found or NULL
+ */
+char *_find_path_(info_t *info, char *pathstr, char *cmd)
+{
+	int i = 0, curr_pos = 0;
+	char *path;
+
+	if (!pathstr)
+		return (NULL);
+	if ((_strlen_(cmd) > 2) && starts_with_(cmd, "./"))
+	{
+		if (_is_cmd_(info, cmd))
+			return (cmd);
+	}
+	while (1)
+	{
+		if (!pathstr[i] || pathstr[i] == ':')
+		{
+			path = _dup_chars_(pathstr, curr_pos, i);
+			if (!*path)
+				_strcat_(path, cmd);
+			else
+			{
+				_strcat_(path, "/");
+				_strcat_(path, cmd);
+			}
+			if (_is_cmd_(info, path))
+				return (path);
+			if (!pathstr[i])
+				break;
+			curr_pos = i;
+		}
+		i++;
+	}
+	return (NULL);
+}
